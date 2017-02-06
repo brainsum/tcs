6bb5b82e567c882e8cd2b20eebfced677450c516
Dániel Décsi <d@niel.hu>
2017-02-06 19:14:11 +0100

    TCS-157: Better flying effect for CTA button

--- web/themes/tieto_admin/src/scripts/TCS-157.js
+++ web/themes/tieto_admin/src/scripts/TCS-157.js
@@ -3,33 +3,6 @@
  * Global scripts, loaded on every page.
  */
 
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-
-
 (function (Drupal, $) {
   'use strict';
 
@@ -37,14 +10,100 @@
     attach: function (context, settings) {
       var latestKnownScrollY = 0;
       var ticking = false;
-      var $cta = $('.paragraph--type--header .field--name-field-call-to-action').once('flying-cta');
+      var $cta = $('.paragraph--type--header .field--name-field-call-to-action .button').once('flying-cta');
+      var $fixed = null;
+      var isFixed = false;
       var classAfterScroll = 'fly';
 
       function update() {
         // reset the tick so we can
         // capture the next onScroll
         ticking = false;
-        $cta.toggleClass(classAfterScroll, latestKnownScrollY > $cta.offset().top - 100);
+
+        var boundingClientRect = $cta[0].getBoundingClientRect();
+        var shouldBeFixed = boundingClientRect.top < 50;
+        var isMobile = window.innerWidth < 768;
+
+        // Create fixed element if it hasnt been created yet.
+        if (shouldBeFixed && !$fixed) {
+          createFixed();
+        }
+
+        // Fix it.
+        if (!isFixed && shouldBeFixed) {
+          // Move button in the place of the original, so we can animate it from there.
+          $fixed
+            .css({
+              position: 'fixed',
+              top: isMobile ? 'auto' : '50%',
+              right: isMobile ? 'auto' : '20px',
+              bottom: isMobile ? '10px' : 'auto',
+              left: isMobile ? 0 : 'auto',
+            });
+
+          var fixedBoundingClientRect = getFixedBoundingClientRect();
+          var diffX = boundingClientRect.left - fixedBoundingClientRect.left;
+          var diffY = boundingClientRect.top - fixedBoundingClientRect.top;
+          $fixed.css('transform', 'translateX(' + diffX + 'px) translateY(' + diffY + 'px)');
+
+          isFixed = true;
+
+          setTimeout(function() {
+            $fixed
+              .css('transform', '')
+              .addClass('in')
+          });
+        }
+        // Go back to normal.
+        else if(isFixed && !shouldBeFixed) {
+          var fixedBoundingClientRect = getFixedBoundingClientRect();
+
+          $fixed
+            .removeClass('in')
+            .css({
+              position: 'absolute',
+              top: boundingClientRect.top + window.scrollY,
+              right: 'auto',
+              left: boundingClientRect.left,
+              transform: 'translateX(' + (fixedBoundingClientRect.left - boundingClientRect.left) + 'px) translateY(' + (fixedBoundingClientRect.top - boundingClientRect.top) + 'px)'
+            })
+
+          setTimeout(function() {
+            $fixed
+              .css('transform', '')
+              .addClass('out');
+
+            $fixed.one('transitionend', function() {
+              isFixed = false;
+              $fixed
+                .removeClass('out')
+                .css({
+                  position: 'absolute',
+                  top: boundingClientRect.top + window.scrollY + 'px',
+                  right: 'auto',
+                  left: boundingClientRect.left + 'px',
+                })
+            })
+          });
+        }
+      }
+
+      function createFixed() {
+        var $clone = $cta
+          .clone()
+            .css('padding', $cta.css('padding'));
+
+        $cta.css('transition', 'none');
+        $cta.css('visibility', 'hidden');
+
+        $fixed = $('<div>')
+          .append($clone)
+          .addClass('fly')
+          .appendTo($('body'));
+      }
+
+      function getFixedBoundingClientRect() {
+        return $fixed[0].getBoundingClientRect();
       }
 
       function onScroll() {
@@ -54,7 +113,7 @@
 
       function requestTick() {
         if (!ticking) {
-          requestAnimationFrame(update);
+          window.requestAnimationFrame(update);
         }
         ticking = true;
       }
