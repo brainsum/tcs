c26e06970eb9db804694b737e173c2d277098ab4
mhavelant <mhavelant@brainsum.com>
2017-03-01 15:55:41 +0100

    Updated the redirect controller so requests from 'localhost' don't get redirected anymore.
    	- Also added a simple markup.

--- web/modules/custom/campaign_pages/src/Controller/RedirectController.php
+++ web/modules/custom/campaign_pages/src/Controller/RedirectController.php
@@ -13,14 +13,23 @@ class RedirectController extends ControllerBase {
 
   /**
    * Redirect to tieto.com if it is non local or dev site (brainsum domain).
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @throws \InvalidArgumentException
+   *
+   * @return array|\Drupal\Core\Routing\TrustedRedirectResponse
+   *   Redirection to tieto.com or a simple markup depending on the environment.
    */
   public function redirectToTietoCom(Request $request) {
     global $base_url;
 
     if ('127.0.0.1' === $request->getClientIp()
       || strpos($base_url, 'brainsum') !== FALSE
+      || strpos($base_url, 'localhost') !== FALSE
     ) {
-      return ['#markup' => ''];
+      return ['#markup' => 'This seems to be a development environment. Redirecting to tieto.com has been disabled.'];
     }
 
     return new TrustedRedirectResponse('https://www.tieto.com');
