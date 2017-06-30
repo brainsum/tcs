7d10d34ac59615033a20b9bbe6f7e2b2e7d7e4ea
Tieto BRAINSUM team <dev@brainsum.com>
2017-06-30 10:33:18 +0000

    update-prod.sh added for proper deployment

---
+++ update-prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This bash script will update the project without installing the development modules, that's why it's "prod"
+git pull && composer install --no-dev -o && cd web && drush updb -y && drush cim -y || exit -1
+CIMTESTRET=$( { drush cim -n; } 2>&1 )
+if [[ $CIMTESTRET == "There are no changes to import"* ]]; then
+  echo "Config import successful.";
+else
+  echo "Config import was not completely successful.";
+  exit -1;
+fi
+drush entity-updates && echo "SUCCESS" || exit -1
