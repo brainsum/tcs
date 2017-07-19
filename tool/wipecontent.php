dd1ff5921e4615df3318bf83f70d1a754e793a3f
Peter Ponya <pedro@brainsum.com>
2017-07-19 11:05:37 +0200

    helper script fix

--- tool/wipecontent.php
+++ tool/wipecontent.php
@@ -4,7 +4,7 @@
  * DO NOT RUN ON PRODUCTION, this WILL REMOVE CONTENT
  **/
 
-$entity_types = ['taxonomy_term','node','paragraphs_type'];
+$entity_types = ['taxonomy_term','node','paragraph'];
 foreach ($entity_types as $entity_type) {
   $query = \Drupal::entityQuery($entity_type);
   $ids = $query->execute();
