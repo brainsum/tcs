eb5fddf496a8d4cd82e4ad7eea90ed819e37db2d
Peter Ponya <pedro@brainsum.com>
2017-07-19 11:01:58 +0200

    helper script for dev sites to remove content

---
+++ tool/wipecontent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Helper drush script for developers to remove paragraph types and content
+ * DO NOT RUN ON PRODUCTION, this WILL REMOVE CONTENT
+ **/
+
+$entity_types = ['taxonomy_term','node','paragraphs_type'];
+foreach ($entity_types as $entity_type) {
+  $query = \Drupal::entityQuery($entity_type);
+  $ids = $query->execute();
+
+  $storage_handler = \Drupal::entityTypeManager()->getStorage($entity_type);
+  $entities = $storage_handler->loadMultiple($ids);
+  $storage_handler->delete($entities);
+}
+
