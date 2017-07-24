d0ea639f5f69a398ab9d68b529c1088b4bb4459a
Peter Zajacz <pzajacz@brainsum.com>
2017-07-24 15:15:45 +0200

    Chart JS fix // Separated doughnut charts

--- web/modules/custom/chart_boxes/js/doughnut-chart.js
+++ web/modules/custom/chart_boxes/js/doughnut-chart.js
@@ -1,22 +1,29 @@
 (function ($) {
 
-  var series = $('.layout--chart-type--doughnut .doughnut-chart--left .field--type-integer').map(function () {
-    return $(this).text();
-  }).get();
+  var doughnutBox = $('.doughnut-chart--right .ct-chart');
+  var doughnutBoxLength = doughnutBox.length;
 
-  var labels = series.map(function(i) {
-    return i + "%";
-  });
+  for (var i=0; i <= (doughnutBoxLength-1); i++ ) {
+    var chartId = doughnutBox[i].id;
 
-  new Chartist.Pie('.ct-chart', {
-    series: series, labels: labels
-  }, {
-    donut: true,
-    donutWidth: 115,
-    donutSolid: true,
-    startAngle: 0,
-    showLabel: true
-  });
+    var series = $($(('#' + chartId)).parents('.doughnut-chart--right')[0]).siblings('.doughnut-chart--left').find('.field--type-integer').map(function () {
+      return $(this).text();
+    }).get();
+
+    var labels = series.map(function(i) {
+      return i + "%";
+    });
+
+    new Chartist.Pie(('#'+ chartId), {
+      series: series, labels: labels
+    }, {
+      donut: true,
+      donutWidth: 115,
+      donutSolid: true,
+      startAngle: 0,
+      showLabel: true
+    });
+  }
 
 })(jQuery);
 
