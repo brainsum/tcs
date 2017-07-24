297837cf8692836f833feac68694bddeb43b65c7
Peter Zajacz <pzajacz@brainsum.com>
2017-07-24 22:27:01 +0200

    Doughnut chart labels fix (JS) & light-grey/grey background fix

--- web/modules/custom/chart_boxes/js/doughnut-chart.js
+++ web/modules/custom/chart_boxes/js/doughnut-chart.js
@@ -5,15 +5,22 @@
 
   for (var i=0; i <= (doughnutBoxLength-1); i++ ) {
     var chartId = doughnutBox[i].id;
-
-    var series = $($(('#' + chartId)).parents('.doughnut-chart--right')[0]).siblings('.doughnut-chart--left').find('.field--type-integer').map(function () {
+    // find chart values & labels
+    var chartValue = $($(('#' + chartId)).parents('.doughnut-chart--right')[0]).siblings('.doughnut-chart--left').find('.field--name-parade-value');
+    var chartLabel = $($(('#' + chartId)).parents('.doughnut-chart--right')[0]).siblings('.doughnut-chart--left').find('.paragraph-inner .field--name-parade-title');
+    // save all chart values to an array
+    var series = chartValue.map(function() {
       return $(this).text();
     }).get();
-
-    var labels = series.map(function(i) {
-      return i + "%";
+    // save all chart label to an array
+    var chartLabels = chartLabel.map(function() {
+      return $(this).text();
+    }).get();
+    // add chart labels to chart values and save in new array
+    var labels = series.map(function(value, index) {
+      return value + chartLabels[index];
     });
-
+    // draw the chart
     new Chartist.Pie(('#'+ chartId), {
       series: series, labels: labels
     }, {
--- web/modules/custom/chart_boxes/js/doughnut-text-chart.js
+++ web/modules/custom/chart_boxes/js/doughnut-text-chart.js
@@ -1,18 +1,13 @@
 
 (function ($) {
-  console.log("hello chart");
 
       var chartBox = $('.layout--chart-type--doughnut-and-text .ct-chart');
       var chartLength = chartBox.length;
-      console.log("chart length:" + chartLength);
 
       for (var i=0; i <= (chartLength-1); i++ ) {
         var chartId = chartBox[i].id;
-        console.log(chartId);
         var chartFill = parseInt($(('#' + chartId)).siblings('.field--name-parade-value')[0].textContent);
-        console.log(chartFill);
         var chartData = [chartFill, (100 - chartFill)];
-        console.log(chartData);
 
         new Chartist.Pie(('#'+ chartId), {
           series: chartData
