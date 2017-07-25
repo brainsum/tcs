ed3897234181639308b988a86bf86aa3ce4113b3
mhavelant <mhavelant@brainsum.com>
2017-07-25 12:10:15 +0200

    Added the -n option to composer install.

--- update-prod.sh
+++ update-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This bash script will update the project without installing the development modules, that's why it's "prod"
-git pull && composer install --no-dev -o && cd web && drush updb -y && drush cim -y || exit -1
+git pull && composer install --no-dev -n -o && cd web && drush updb -y && drush cim -y || exit -1
 CIMTESTRET=$( { drush cim -n; } 2>&1 )
 if [[ $CIMTESTRET == "There are no changes to import"* ]]; then
   echo "Config import successful.";
