f529514ba2abefb1a72f1b9bec6d843ebe10e216
Viktor <vjaszai@brainsum.com>
2018-01-11 16:17:06 +0100

    Update added for docker users

---
+++ update-nogitcomposer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd web && \
+ ../vendor/bin/drush updb -y \
+ && ../vendor/bin/drush cim sync -y \
+ && ../vendor/bin/drush entity-updates -y \
+ && ../vendor/bin/drush cr \
+ && echo "Successfully updated."
