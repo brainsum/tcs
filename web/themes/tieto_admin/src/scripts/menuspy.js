4d8888f89dce1c5bc2260fd1110a90f65f1da454
Viktor <vjaszai@brainsum.com>
2018-01-14 21:32:03 +0100

    TCS-474 | Menu now showing current element

---
+++ web/themes/tieto_admin/src/scripts/menuspy.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Menu highlighting while scroll
+ *
+ * @see  https://github.com/lcdsantos/menuspy
+ */
+
+import MenuSpy from 'menuspy';
+
+var menu = document.querySelector('#nav-menu-link-list');
+var menuspy = new MenuSpy(menu);
