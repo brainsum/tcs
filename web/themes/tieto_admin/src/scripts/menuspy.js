783eab9be56163d77a8c6070315064e1d64c7432
Viktor <vjaszai@brainsum.com>
2018-01-16 15:26:37 +0100

    TCS-474 | Menuspy changes

--- web/themes/tieto_admin/src/scripts/menuspy.js
+++ web/themes/tieto_admin/src/scripts/menuspy.js
@@ -7,5 +7,23 @@
 
 import MenuSpy from 'menuspy';
 
-var menu = document.querySelector('#nav-menu-link-list');
-var menuspy = new MenuSpy(menu);
+var $menu = jQuery('#nav-menu-link-list');
+
+var menuspy = new MenuSpy($menu[0],{
+  enableLocationHash: false,
+  threshold: 15,
+  callback: function() {
+    console.log('callback')
+  }
+});
+
+var $lis = $menu.find('>li');
+
+$menu.find('>li>a').on('click', function(e) {
+  var $this = jQuery(this);
+
+  $lis.removeClass('active');
+
+  $this.parent().addClass('active'); 
+});
+
