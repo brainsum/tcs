eaf8beceee1f8bee122173e29b811430fea1522d
Viktor <vjaszai@brainsum.com>
2018-01-30 10:51:44 +0100

    TCS-474 | Menuspy source added

--- web/themes/tieto_admin/src/scripts/menuspy.js
+++ web/themes/tieto_admin/src/scripts/menuspy.js
@@ -11,7 +11,7 @@ var $menu = jQuery('#nav-menu-link-list');
 
 var menuspy = new MenuSpy($menu[0],{
   enableLocationHash: false,
-  threshold: 0,
+  threshold: 15,
   callback: function() {
     console.log('callback')
   }
