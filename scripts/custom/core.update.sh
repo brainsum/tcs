46cd706b81caa258288cac2800aef780ee4b4ad4
szato <tszanyi@brainsum.com>
2018-05-10 10:31:01 +0200

    Added 'drush cr' after updb and cim.

--- scripts/custom/core.update.sh
+++ scripts/custom/core.update.sh
@@ -4,6 +4,8 @@ echo "Executing core for update.sh"
 composer install \
  && cd web \
  && drush updb -y \
+ && drush cr \
  && drush cim sync -y \
+ && drush cr \
  && drush entity-updates -y \
  && echo "Successfully updated."
