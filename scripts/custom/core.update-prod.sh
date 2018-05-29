e8d02d8ba16e6753db80d1c99723711ec392781a
mhavelant <mhavelant@brainsum.com>
2018-05-29 10:59:46 +0200

    Add post-composer cache rebuild to the release scripts.

--- scripts/custom/core.update-prod.sh
+++ scripts/custom/core.update-prod.sh
@@ -2,16 +2,16 @@
 
 echo "Executing core for update-prod.sh"
 composer install --no-dev -o \
- && cd web \
- && drush updb -y \
- && drush cr \
- && drush cim sync -y \
- && drush cr \
- && drush entity-updates -y \
- || exit -1
+    && cd web \
+    && drush updb -y \
+    && drush cr \
+    && drush cim sync -y \
+    && drush cr \
+    && drush entity-updates -y \
+    || exit -1
 
 CIMTESTRET=$( { drush cim -n; } 2>&1 )
-if [[ $CIMTESTRET == "There are no changes to import"* ]]; then
+if [[ ${CIMTESTRET} == "There are no changes to import"* ]]; then
   echo "Config import successful.";
 else
   echo "Config import was not completely successful.";
--- scripts/custom/core.update.sh
+++ scripts/custom/core.update.sh
@@ -2,10 +2,11 @@
 
 echo "Executing core for update.sh"
 composer install \
- && cd web \
- && drush updb -y \
- && drush cr \
- && drush cim sync -y \
- && drush cr \
- && drush entity-updates -y \
- && echo "Successfully updated."
+    && cd web \
+    && drush cr \
+    && drush updb -y \
+    && drush cr \
+    && drush cim sync -y \
+    && drush cr \
+    && drush entity-updates -y \
+    && echo "Successfully updated."
