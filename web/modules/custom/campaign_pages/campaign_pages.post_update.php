8c528f710da8b5c2ebc794012734423d9bc87bc4
Ilma Kosa <kosailma99@gmail.com>
2019-10-31 17:22:50 +0200

    TCS-698 | Added post update hook for manually adding the moderation_state field to taxonomy terms.

--- web/modules/custom/campaign_pages/campaign_pages.post_update.php
+++ web/modules/custom/campaign_pages/campaign_pages.post_update.php
@@ -11,6 +11,7 @@ use Drupal\campaign_pages\Helper\ScheduledUpdateState2WorkbenchModeration;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Database\StatementInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 
 /**
  * Re-save classy paragraphs.
@@ -657,4 +658,36 @@ function campaign_pages_post_update_8406() {
     NULL,
     'layout_bar_chart'
   );
+}
+
+/**
+ * Manually add the moderation_state field to taxonomy terms post 8.7 update.
+ */
+function campaign_pages_post_update_add_taxonomy_moderation() {
+  $definition_update_manager = \Drupal::entityDefinitionUpdateManager();
+  /** @var \Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface $last_installed_schema_repository */
+  $last_installed_schema_repository = \Drupal::service('entity.last_installed_schema.repository');
+  $entity_type = $definition_update_manager->getEntityType('taxonomy_term');
+  if (($fields = $last_installed_schema_repository->getLastInstalledFieldStorageDefinitions($entity_type->id())) && empty($fields['moderation_state'])) {
+    $definition = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Moderation state'))
+      ->setDescription(t('The moderation state of this piece of content.'))
+      ->setSetting('target_type', 'moderation_state')
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'hidden',
+        'weight' => -5,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'moderation_state_default',
+        'weight' => 5,
+        'settings' => [],
+      ])
+      ->addConstraint('ModerationState', [])
+      ->setDisplayConfigurable('form', FALSE)
+      ->setDisplayConfigurable('view', FALSE);
+    $definition_update_manager->installFieldStorageDefinition('moderation_state', $entity_type->id(), 'workbench_moderation', $definition);
+  }
 }
