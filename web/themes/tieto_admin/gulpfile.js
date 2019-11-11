5e4cbd3ceaa669870d25bedae73ee1a6b0ba777b
Krisztian Pinter <kpinter@brainsum.com>
2019-11-11 17:36:14 +0100

    TCS-699 | Removed Helvetica Neue font and refactored gulp tasks.

--- web/themes/tieto_admin/gulpfile.js
+++ web/themes/tieto_admin/gulpfile.js
@@ -1,26 +1,75 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const imagemin = require('gulp-imagemin');
-const sourcemaps = require('gulp-sourcemaps');
-const Parcel = require('parcel-bundler');
+const browserSync       = require('browser-sync').create();
+const gulp              = require('gulp');
+const autoprefixer      = require('autoprefixer');
+const postcss           = require('gulp-postcss');
+const sass              = require('gulp-sass');
+const imagemin          = require('gulp-imagemin');
+const sourcemaps        = require('gulp-sourcemaps');
+const Parcel            = require('parcel-bundler');
 
-const paths = {
-  scripts: {
-    src: 'src/js/*.js',
-    dest: 'js',
+// Define settings
+const config = {
+  paths: {
+    scripts: {
+      src: 'src/js/*.js',
+      dest: 'js',
+    },
+    styles: {
+      src: './src/scss/*.scss',
+      dest: 'css',
+    },
+    images: {
+      src: 'src/img/*',
+      dest: 'images',
+    }
   },
-  styles: {
-    src: 'src/scss/*.scss',
-    dest: 'css',
-  },
-  images: {
-    src: 'src/img/*',
-    dest: 'images',
+  browserSync: {
+    proxy: 'https://tcs.test',
+    autoOpen: false,
+    browsers: [
+      'Google Chrome',
+    ],
   }
+};
+
+// Styles Task - Dev version
+function stylesDev(done) {
+  gulp
+    .src(config.paths.styles.src)
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(sass({
+      outputStyle: 'expanded',
+      precision: 10,
+      linefeed: 'lf'
+    }))
+    .on('error', sass.logError)
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write({ includeContent: false }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.paths.styles.dest))
+    .pipe(browserSync.stream());
+  done();
 }
 
+// Styles Task - Prod version
+function stylesProd(done) {
+  gulp
+    .src(config.paths.styles.src)
+    .pipe(sass({
+      outputStyle: 'compact',
+      precision: 10,
+      linefeed: 'lf'
+    }))
+    .on('error', sass.logError)
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest(config.paths.styles.dest));
+  done();
+}
+
+// Scripts (Parcel) Task
 async function scripts() {
-  const parcel = new Parcel(paths.scripts.src, {
+  const parcel = new Parcel(config.paths.scripts.src, {
     watch: true,
     minify: true,
     outDir: './js',
@@ -34,32 +83,37 @@ async function scripts() {
   return await parcel.bundle();
 }
 
-function styles() {
-  return gulp.src(paths.styles.src, {sourcemaps: true})
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded',
-      linefeed: 'lf'
-    }).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.styles.dest));
+// Image Optimization Task
+function images(done) {
+ gulp
+   .src(config.paths.images.src)
+   .pipe(imagemin())
+   .pipe(gulp.dest(config.paths.images.dest));
+ done();
 }
 
-function images() {
-  return gulp.src(paths.images.src)
-    .pipe(imagemin())
-    .pipe(gulp.dest(paths.images.dest))
+// BrowserSync Task
+function browserSyncWatch(done) {
+  browserSync.init({
+    proxy: config.browserSync.proxy,
+    open: config.browserSync.autoOpen,
+    browser: config.browserSync.browsers,
+  });
+  gulp.watch(config.paths.styles.src, stylesDev);
+  gulp.watch(config.paths.scripts.src, scripts);
+  done();
 }
 
-function watch() {
-  scripts();
-  gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.images.src, images);
+// BrowserSync Reload Task
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
 }
 
-exports.styles = styles;
-exports.scripts = scripts;
-exports.images = images;
-exports.watch = watch;
+// Define complex tasks
+const compileDev = gulp.parallel(stylesDev, scripts, images);
+const compileProd = gulp.parallel(stylesProd, scripts, images);
 
-exports.default = watch;
+// export tasks
+exports.default = gulp.series(compileDev, browserSyncWatch);
+exports.prod = compileProd;
