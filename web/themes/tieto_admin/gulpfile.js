976600d2c2e1e65126826a3a81aea43ba2ca7648
Krisztian Pinter <keeteean@gmail.com>
2019-11-11 13:54:35 +0100

    TCS-699 | Fixed imagemin sec. hole'.

--- web/themes/tieto_admin/gulpfile.js
+++ web/themes/tieto_admin/gulpfile.js
@@ -5,48 +5,56 @@ const sourcemaps = require('gulp-sourcemaps');
 const Parcel = require('parcel-bundler');
 
 const paths = {
-    scripts: {
-        src: 'src/js/*.js',
-        dest: 'js',
-    },
-    styles: {
-        src: 'src/scss/*.scss',
-        dest: 'css',
-    },
-    images: {
-        src: 'src/img/*',
-        dest: 'images',
-    }
+  scripts: {
+    src: 'src/js/*.js',
+    dest: 'js',
+  },
+  styles: {
+    src: 'src/scss/*.scss',
+    dest: 'css',
+  },
+  images: {
+    src: 'src/img/*',
+    dest: 'images',
+  }
 }
 
 async function scripts() {
-    const parcel = new Parcel(paths.scripts.src, {watch: true, minify: true, outDir: './js', publicUrl: './'});
+  const parcel = new Parcel(paths.scripts.src, {
+    watch: true,
+    minify: true,
+    outDir: './js',
+    publicUrl: './'
+  });
 
-    parcel.on('buildEnd', () => {
-        console.log('Scripts bundled.')
-    });
+  parcel.on('buildEnd', () => {
+    console.log('Scripts bundled.')
+  });
 
-    return await parcel.bundle();
+  return await parcel.bundle();
 }
 
-function styles () {
-    return gulp.src(paths.styles.src, { sourcemaps: true })
+function styles() {
+  return gulp.src(paths.styles.src, {sourcemaps: true})
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded', linefeed: 'lf' }).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded',
+      linefeed: 'lf'
+    }).on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 function images() {
-    return gulp.src(paths.images.src)
+  return gulp.src(paths.images.src)
     .pipe(imagemin())
     .pipe(gulp.dest(paths.images.dest))
 }
 
 function watch() {
-    scripts();
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.images.src, images);
+  scripts();
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.images.src, images);
 }
 
 exports.styles = styles;
